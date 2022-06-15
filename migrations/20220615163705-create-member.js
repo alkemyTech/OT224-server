29c79101b8d23a10aad2dcf0d582c483bd586cc7
ChristianLeiva <chriistiianleiiva@gmail.com>
2022-06-15 13:56:54 -0300

    added detedAt

--- migrations/20220615163705-create-member.js
+++ migrations/20220615163705-create-member.js
@@ -26,6 +26,9 @@ module.exports = {
       description: {
         type: Sequelize.STRING
       },
+      deletedAt: {
+        type: Sequelize.DATE
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE

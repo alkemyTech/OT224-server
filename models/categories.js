5ada17882b45c2e8d48a7855885e8f4582daafd4
SLRonzoni <slronzoni@gmail.com>
2022-06-15 19:46:34 -0300

    add files , create categories model, migration and controller

---
+++ models/categories.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Categories extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Categories.init({
+    name: { type: DataTypes.STRING, 
+            allowNull: false },
+
+    description: { type: DataTypes.STRING, 
+                   allowNull: true },
+
+    image: { type: DataTypes.STRING, 
+             allowNull: true },
+             
+    deletedAt: DataTypes.DATE,
+  }, {
+    sequelize,
+    modelName: 'Categories',
+  });
+  return Categories;
+};

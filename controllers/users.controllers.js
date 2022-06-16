eed6e732763874230d83f7f6d6cc88d5a8726198
ChristianLeiva <chriistiianleiiva@gmail.com>
2022-06-15 21:13:26 -0300

    correciones se puso el middleware en una carperta separada a nivel de controller y se elimino un metodo innecesario en ruta

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,20 +1,5 @@
 const ModeloUser = require('../models').User;
 
-const verifyIsAdmin = async(req, res, next) =>{
-    const userData =  await ModeloUser.findOne({where: {firstname: req.body.firstname}})
-    if(userData){
-       if(userData.roleId === 1){
-        next()
-       }else{
-        res.send("No tiene los permisos necesarios")
-       }
-    }else{
-        res.send("no existe el usuario con ese nombre")
-    }
-
-    
-}
-
 const getAllUsers = async(req, res) =>{
     const users =  await ModeloUser.findAll()
     res.send(users)
@@ -22,6 +7,5 @@ const getAllUsers = async(req, res) =>{
 }
 
 module.exports = {
-    verifyIsAdmin,
     getAllUsers
 }
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const userController = require('../controllers/users.controllers')
-const verifyIsAdmin = require('../controllers/users.controllers').verifyIsAdmin
-
 
 /* GET users listing. */
-router.get('/', verifyIsAdmin, userController.getAllUsers)
-
-/* verify if user is admin */
-
-router.get('/verify', userController.verifyIsAdmin)
-
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
 module.exports = router;

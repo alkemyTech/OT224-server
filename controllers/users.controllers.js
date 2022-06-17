04ba4e8c717d2161730304a5c00bbbd326039ec1
FlorenciaAGarrido <flhor.garrido@gmail.com>
2022-06-17 14:05:17 -0300

    (feat(OT224-37): user authenticated service implementation

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,35 +1,50 @@
-const ModeloUser = require('../models').User;
+const ModeloUser = require("../models").User;
 
-const getAllUsers = async(req, res) =>{
-    try {
-        const users =  await ModeloUser.findAll()
-        if(!users){
-            res.status(400).json({
-                msg: 'users not found'
-            })
-        }else{
-            res.status(200).send(users)
-        }
-    } catch (error) {
-        res.status(500).send(error)       
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await ModeloUser.findAll();
+    if (!users) {
+      res.status(400).json({
+        msg: "users not found",
+      });
+    } else {
+      res.status(200).send(users);
     }
-}
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
-const createUser = async (req, res) =>{
-    res.send('hello from create user')
-}
+const createUser = async (req, res) => {
+  res.send("hello from create user");
+};
 
-const updateUser = async (req, res) =>{
-    res.send('Hello from update user')
-}
+const updateUser = async (req, res) => {
+  res.send("Hello from update user");
+};
 
-const deleteUser = async (req, res) =>{
-    res.send('Hello from delete user')
-}
+const deleteUser = async (req, res) => {
+  res.send("Hello from delete user");
+};
+
+const findUserById = async (req, res, next) => {
+  try {
+    return res.status(200).send(await ModeloUser.findByPk(req.params.id));
+  } catch (error) {
+    console.log(error);
+    next(err);
+  }
+};
+
+const findMe = async (req, res, next) => {
+  return res.status(200).send(res.user);
+};
 
 module.exports = {
-    getAllUsers,
-    createUser,
-    updateUser,
-    deleteUser
-}
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+  findUserById,
+  findMe,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const userController = require('../controllers/users.controllers');
-const { authenticatedUser } = require('../middlewares/authenticatedUser');
-
+const userController = require("../controllers/users.controllers");
+const { authenticatedUser } = require("../middlewares/authenticatedUser");
 
 /* GET users listing. */
-router.get('/', authenticatedUser, userController.getAllUsers )
+router.get("/", authenticatedUser, userController.getAllUsers);
 // create user
-router.post('/create', userController.createUser)
+router.post("/create", userController.createUser);
 //update user
-router.put('/update/:id', userController.updateUser)
+router.put("/update/:id", userController.updateUser);
 //delete user
-router.delete('/delete/:id',userController.deleteUser)
+router.delete("/delete/:id", userController.deleteUser);
 
 module.exports = router;
-

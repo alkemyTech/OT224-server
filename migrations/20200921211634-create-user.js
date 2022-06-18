0b7d4fbee85c98ad984f2c10b16a782a55ffd9f3
hugotata78 <hugotatarinoff@gmail.com>
2022-06-18 13:19:23 -0300

    validaciones

--- migrations/20200921211634-create-user.js
+++ migrations/20200921211634-create-user.js
@@ -9,18 +9,23 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       firstName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull:false,
       },
       lastName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull:false,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull:false,
+        unique:true,
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull:false,
       },
-      image: {
+      photo: {
         type: Sequelize.STRING
       },
       roleId: {
--- validators/validatorRegister.js
+++ validators/validatorRegister.js
@@ -1,14 +1,42 @@
 const { check } = require('express-validator');
 
 
+
 const validateRegister = [
-    check('firstName').isAlpha()
-        .isLength({ min: 3 }),
-    check('lastName').isAlpha()
-        .isLength({ min: 3 }),
-    check('email').isEmail(),
-    check('password').isAlphanumeric()
-        .isLength({ min: 6 }),
+    check('firstName')
+        .not()
+        .isEmpty()
+        .withMessage('Please enter your first name'),
+    check('firstName')
+        .isAlpha()
+        .withMessage('The first name can only contain letters'),
+    check('firstName')
+        .isLength({ min: 3 })
+        .withMessage('First name must contain 3 or more characters'),
+    check('lastName')
+        .not()
+        .isEmpty()
+        .withMessage('Please enter your last name'),
+    check('lastName')
+        .isAlpha()
+        .withMessage('The last name can only contain letters'),
+    check('lastName')
+        .isLength({ min: 3 })
+        .withMessage('Last name must contain 3 or more characters'),
+    check('email')
+        .not()
+        .isEmpty()
+        .withMessage('Please enter your email'),
+    check('email')
+        .isEmail()
+        .withMessage('Enter a valid email'),
+    check('password')
+        .not()
+        .isEmpty()
+        .withMessage('Please enter a password'),
+    check('password')
+        .isLength({ min: 6 })
+        .withMessage('The password must contain 3 or more characters'),
 ]
 
 module.exports = { validateRegister }

3cf56c8301dee0a2d5c197882435db48b778bb28
FlorenciaAGarrido <flhor.garrido@gmail.com>
2022-06-20 18:36:50 -0300

    fix(OT224-21): model and migration activity implementation

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,61 +1,64 @@
 const ActivityModel = require("../models").Activity;
 
-const getAllActivities = async (req, res, next) => {
-  try {
-    const activities = await ActivityModel.findAll();
-    return res.status(200).send(activities);
-  } catch (error) {
-    console.log(error);
-    next(err);
-  }
+const getAllActivities = async (req, res) => {
+	try {
+		const activities = await ActivityModel.findAll();
+		return res.status(200).json(activities);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
 };
 
 const getActivityById = async (req, res) => {
-  try {
-    const a = await ActivityModel.findByPk(req.params.id);
-    return res.status(200).send(a);
-  } catch (err) {
-    next(err);
-  }
+	try {
+		const a = await ActivityModel.findByPk(req.params.id);
+		return res.status(200).json(a);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
 };
 
 const createActivity = async (req, res) => {
-  try {
-    let a = req.body;
-    a = await ActivityModel.create(a);
-    return res.status(201).send(a);
-  } catch (err) {
-    next(err);
-  }
+	try {
+		let a = req.body;
+		a = await ActivityModel.create(a);
+		return res.status(201).json(a);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
 };
 
 const updateActivity = async (req, res) => {
-  try {
-    const id = req.params.id;
-    const a = await ActivityModel.update(req.body, { where: { id } });
-    return res.status(201).send(a);
-  } catch (err) {
-    next(err);
-  }
+	try {
+		const id = req.params.id;
+		await ActivityModel.update(req.body, { where: { id } })
+		const activity = await ActivityModel.findByPk(id)
+		res.status(200).json(activity)
+
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
 };
 
 const deleteActivity = async (req, res) => {
-  try {
-    const id = req.params.id;
-    const a = await ActivityModel.update(
-      { deletedAt: new Date() },
-      { where: { id } }
-    );
-    return res.status(200).send(a);
-  } catch (err) {
-    next(err);
-  }
+	try {
+		const id = req.params.id;
+		await ActivityModel.destroy({ where: { id } })
+		res.status(200).json(true)
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
 };
 
 module.exports = {
-  getAllActivities,
-  getActivityById,
-  createActivity,
-  updateActivity,
-  deleteActivity,
+	getAllActivities,
+	getActivityById,
+	createActivity,
+	updateActivity,
+	deleteActivity,
 };

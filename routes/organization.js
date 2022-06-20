52a9cdfe1ba7f0531326300caf33fd799d3298ce
Kevin <k.joseph.90@hotmail.com>
2022-06-20 17:07:24 -0500

    Se creó el endpoint para actualizar los datos de la organización

--- routes/organization.js
+++ routes/organization.js
@@ -1,10 +1,13 @@
+const { validateOrganization } = require("../validators/organizationValidator");
 const organization = require("../controllers/OrganizationController");
+const { authenticatedUser } = require("../middlewares/authenticatedUser");
+const { verifyIsAdmin } = require("../middlewares/user.middelware")
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 /* POST create organization. */
-router.post('/create', organization.createOrganization);
+router.post('/create', validateOrganization, organization.createOrganization);
 
 router.get('/public', organization.getOrganization);
-
+router.put('/public/:id', authenticatedUser, verifyIsAdmin, validateOrganization, organization.updateOrganization);
 module.exports = router;

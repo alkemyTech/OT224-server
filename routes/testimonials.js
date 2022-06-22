13d3961f8c393fd022bf2da3f4aea8b4e192c271
Kevin <k.joseph.90@hotmail.com>
2022-06-22 12:12:54 -0500

    Endpoint creación Testimonios

--- routes/testimonials.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let router = express.Router();
-const controller = require('../controller/testimonials');
-
-/* GET testimonials listen. */
-router.get('/', controller.get);
-
-module.exports = router;

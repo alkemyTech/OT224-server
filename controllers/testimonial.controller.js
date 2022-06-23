ec9832b511307522b1388b4138918fab1743fe71
Kevin <k.joseph.90@hotmail.com>
2022-06-23 10:10:12 -0500

    Se creo la lógica para obetener todos los testimonials y se creó el endpoint para actualizar un testimonial

--- controllers/testimonial.controller.js
+++ controllers/testimonial.controller.js
@@ -14,9 +14,42 @@ const createTestimonial = async (req, res) => {
 
 //Get testimonials
 const getAllTestimonials = async (req, res) => {
-    res.send("Hello from GetAllTestimonials")
+    try {        
+        const testimonials = await TestimonialModel.findAll();
+        if(!testimonials){
+            res.status(404).send({message: "There is no testimonials"})
+        }else{            
+            res.status(200).send(testimonials);
+        }        
+    } catch (error) {
+        res.status(500).send(error);
+    }
 }
+
+//Update testimonial
+const updateTestimonial = async (req, res) => {
+    try {
+        const id = req.params.id
+        const testimonial = await TestimonialModel.findByPk(id);
+        
+        if(!testimonial) {
+            res.status(404).send({message: "Testimonial no found!"});
+        }else{
+            const updateTestimonial = await TestimonialModel.update(req.body, { where: { id } });
+
+            if (updateTestimonial == 1){
+                const updatedTestimonial = await TestimonialModel.findByPk(id);
+                res.status(201).send(updatedTestimonial);
+            }
+        }
+
+    } catch (error) {        
+        res.status(500).send(error);
+    }
+}
+
 module.exports = {
     createTestimonial,
-    getAllTestimonials
+    getAllTestimonials,
+    updateTestimonial
 }

0a8eb283c1947a6758c3112f512090edd7af74e1
kany west <luis-94@live.com>
2022-06-23 09:06:20 -0300

    se borra testimonials duplicado

--- routes/testimonials.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let router = express.Router();
-const controller = require('../controllers/testimonials.controller');
-
-/* GET testimonials listen. */
-router.get('/', controller.get);
-
-module.exports = router;

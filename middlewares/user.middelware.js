0da8744ef9a2ebfae73e69a1a9b0545f5ae04c4b
ChristianLeiva <chriistiianleiiva@gmail.com>
2022-06-23 13:59:48 -0300

    se le agrego una funcion para eliminar la palabra baerer y de modifico tambien para que funcione con los ultimos cambios del token

--- middlewares/user.middelware.js
+++ middlewares/user.middelware.js
@@ -1,7 +1,14 @@
 const jwt = require('jsonwebtoken')
+const ADMIN_ROLE_ID = require('../sharedConstants')
+
 
 const verifyIsAdmin = (req, res, next) =>{
     const token = req.headers['authorization']
+
+    
+    if (token && token.toLowerCase().startsWith('bearer')) {
+        token = token.substring(7)
+      }   
 
     if (!token) {
         return res.status(403).send('invalid or nonexistent token')
@@ -9,7 +16,7 @@ const verifyIsAdmin = (req, res, next) =>{
       
     try {        
         const decodedToken = jwt.verify(token, process.env.PRIVATE_KEY)
-        if(decodedToken.roleId === 1){
+        if(decodedToken.roleId === ADMIN_ROLE_ID){
             next()
         }else{
             res.send("you do not have the necessary permissions")

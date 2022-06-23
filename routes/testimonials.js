55d303f345d60853fc41d2070a1d609e4c3bc4ab
kany west <luis-94@live.com>
2022-06-23 00:13:12 -0300

    agregnado cambios

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let router = express.Router();
-const controller = require('../controller/testimonials');
+const controller = require('../controllers/testimonials.Controller');
 
 /* GET testimonials listen. */
 router.get('/', controller.get);

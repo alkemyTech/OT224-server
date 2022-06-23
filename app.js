a42c6b087268a3fb022261f2c5ab4c0610f20bd9
kany west <luis-94@live.com>
2022-06-23 00:31:29 -0300

    arreglando cambios

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('./testimonials',testimonialsRouter)
+app.use('/testimonials',testimonialsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

cbd33a562127a4745dc5f84cfa60d3a63372f48f
kany west <luis-94@live.com>
2022-06-24 18:40:03 -0300

    arreglo de cambios en el modelo y controller

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -3,24 +3,12 @@ const newsModel = require('../models').News;
 
 const createNews = async (req,res) =>{
     try{
-        const getNews = await newsModel.findOne({
-            where: {
-                name: req.body.name
-            }
-        })
-        if(getNews){
-            res.status(400).send({
-                message:'Ya existe en la base de datos', 
-                status:400
-            })
-        }else {
         let news = req.body;
         news = await newsModel.create(news)
         res.status(201).send({
             data:news,
             status:200
         });
-    }
     } catch (error) {
         res.status(500).send(error);
     }

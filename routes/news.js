5c8e75bcc146966a601cdb54aef2059579f51054
kany west <luis-94@live.com>
2022-06-24 08:36:40 -0300

    agregando autenticacion de usuario por ruta post

--- routes/news.js
+++ routes/news.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express();
 const newsController = require('../controllers/news.controller');
-const {validateNews} = require('../validators/newsValidator');
+const { validateNews } = require('../validators/newsValidator');
+const { authenticatedUser } = require('../middlewares/authenticatedUser');
+const { verifyIsAdmin } = require('../middlewares/user.middelware');
 
-router.post('/', validateNews ,newsController.createNews);
+// Create News
+router.post('/', authenticatedUser, verifyIsAdmin ,validateNews ,newsController.createNews);
 
 module.exports = router;

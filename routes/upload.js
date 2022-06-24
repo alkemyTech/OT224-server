f3edbd3446b67ab984d4d72c424db62266cad528
Victor Amarilla <95294380+victor13102000@users.noreply.github.com>
2022-06-24 14:27:15 -0300

    Delete upload.js
    Eliminación de ruta innecesaria

--- routes/upload.js
+++
@@ -1,8 +0,0 @@
-const express= require('express');
-const router = express();
-const uploadControllers= require('../controllers/upload.controller')
-const verifyFile = require('../middlewares/verifyFile')
-
-router.post('/',verifyFile, uploadControllers)
-
-module.exports= router

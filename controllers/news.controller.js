49787df5f67e3a01b235b2c76734e2555a157d01
kany west <luis-94@live.com>
2022-06-27 09:10:51 -0300

    Detalle de news

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -14,7 +14,25 @@ const createNews = async (req,res) =>{
     }
 }
 
+const detailNews = async (req,res) =>{
+    try{
+        news = await newsModel.findByPk(req.params.id);
+        if(!news){
+            res.status(404).send({
+            message: "News no found!", 
+            status:404
+        });
+        }else {
+            res.status(200).send({
+                data:news, 
+                status:200
+            }); 
+        }
+    } catch (error) {res.status(500).send(error);}
+}
+
 
 module.exports = {
     createNews,
+    detailNews
 }

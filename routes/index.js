677a37efd5c1fbd3f83917dabcd29e855cc9906e
Kevin <k.joseph.90@hotmail.com>
2022-06-27 08:56:46 -0500

    Implementación de la librería Swagger

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 const usersRouter = require('./users');
 const organizationRouter = require('./organizations.routes');
 const testimonialsRouter = require('./testimonials.routes');
@@ -10,12 +13,30 @@ const membersRouter = require('./members')
 const rolesRouter = require('./role');
 const activitiesRouter = require('./activities');
 const slidesRouter = require('./slides');
-const contactRoutes = require('./contacts.routes')
+const contactRoutes = require('./contacts.routes');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+const swaggerSpec = {
+  definition: {
+    openapi: '3.0.2',
+    info: {
+      title: 'ONG Somos más API',
+      description: 'API Documentation for use',
+      version: '1.0.0',
+    },
+    servers: [
+        {
+            url:"http://localhost:3000"
+        }
+    ]
+  },
+  // looks for configuration in specified directories
+  apis: ['./routes/*.js'],
+};
 
 router.use('/users', usersRouter);
 router.use('/organization', organizationRouter);
@@ -27,6 +48,7 @@ router.use('/members', membersRouter )
 router.use('/roles', rolesRouter);
 router.use('/activities', activitiesRouter);
 router.use('/slides', slidesRouter);
-router.use('/contacts',contactRoutes)
+router.use('/contacts',contactRoutes);
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(swaggerSpec)));
 
 module.exports = router;

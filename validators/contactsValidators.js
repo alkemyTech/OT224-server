e3b2a47562eb831ea98a0c0cbd0af7f98a92a8fc
hugotata78 <hugotatarinoff@gmail.com>
2022-06-27 13:56:06 -0300

    validación para cotejar que el email sea único

--- validators/contactsValidators.js
+++ validators/contactsValidators.js
@@ -1,5 +1,6 @@
 const { check } = require('express-validator')
 const { validateResult } = require("../helpers/validate");
+const Contacts = require('../models').Contacts
 
 const contactsValidators = [
     check('name')
@@ -12,6 +13,16 @@ const contactsValidators = [
         .withMessage('Please enter your email!')
         .isEmail()
         .withMessage('Please enter a valid email!'),
+    check('email').custom(async (email) => {
+        const contact = await Contacts.findOne({
+            where: {
+                email: email
+            }
+        })
+        if (contact) {
+            throw new Error('Email already registered')
+        }
+    }),
 
     (req, res, next) => {
         validateResult(req, res, next)

d6ff4bcd58a17ae7a40d1f28121ffc7bfd539f8c
FlorenciaAGarrido <flhor.garrido@gmail.com>
2022-06-27 23:03:40 -0300

    feat(ot224-54):activities seeders create

--- seeders/20220616232349-demo-activity.js
+++ seeders/20220616232349-demo-activity.js
@@ -5,7 +5,21 @@ module.exports = {
     await queryInterface.bulkInsert('Activities', [{
       name: 'Activity Demo',
       content: 'Content Demo',
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      image: 'https://busytoddler.com/wp-content/uploads/2020/03/bigkid-activities.jpg',
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {
+      name: 'Activity Demo 2',
+      content: 'Content Demo 2',
+      image: 'https://www.peanuts.com/sites/default/files/takecare_familycenter_activities.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {
+      name: 'Activity Demo 3',
+      content: 'Content Demo 3',
+      image: 'https://static.meraevents.com/content/categorylogo/Activities-thumb1582184387.jpg',
       createdAt: new Date,
       updatedAt: new Date
     }], {});

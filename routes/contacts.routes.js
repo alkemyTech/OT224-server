2ef9d55fe6c086cbc0035703af7be049d5667a2d
hugotata78 <hugotatarinoff@gmail.com>
2022-06-30 17:55:35 -0300

    endpoint para editar contactos

--- routes/contacts.routes.js
+++ routes/contacts.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { createContact, getAllContacts, deleteContact } = require('../controllers/contact.controller')
+const { createContact, getAllContacts, deleteContact, updateContact } = require('../controllers/contact.controller')
 const { authenticatedUser } = require('../middlewares/authenticatedUser')
 const { verifyIsAdmin } = require('../middlewares/user.middelware')
 const { contactsValidators } = require('../validators/contactsValidators')
@@ -9,5 +9,6 @@ const router = Router()
 router.post('/',  contactsValidators, createContact)
 router.get('/', authenticatedUser, verifyIsAdmin, getAllContacts)
 router.delete('/:id', authenticatedUser, verifyIsAdmin, deleteContact)
+router.put('/:id', authenticatedUser, verifyIsAdmin, updateContact)
 
 module.exports = router

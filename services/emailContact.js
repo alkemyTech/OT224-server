f0dfc24ba487163252e1bdda8f80185a7c00520b
hugotata78 <hugotatarinoff@gmail.com>
2022-07-01 11:25:09 -0300

    archivo de configuración envió de email al crear contacto

---
+++ services/emailContact.js
@@ -0,0 +1,31 @@
+const { send } = require("../services/emails");
+const ejs = require("ejs");
+
+
+  const emailContact = async(contact) => {
+    
+    try{
+        const output = await ejs.renderFile("./views/emailContact.ejs", {name: contact.name}); 
+        if(output) {           
+          const to=contact.email;
+          const subject= `Bienvenid@ a Somos Más 🙌  !!! `;
+          const body= output;
+          console.log(body)
+
+          await send(to,subject,body);
+          return true
+
+        } else { 
+          return false
+        }
+    } catch (error) {
+        console.log(error)
+        return false   
+    }
+
+ }  
+
+ 
+
+
+module.exports={ emailContact };

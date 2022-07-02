97c7a13512d9f06c3c71b2e1b83e57e5c19351ed
kany west <luis-94@live.com>
2022-07-01 21:03:49 -0300

    endpoint getAllNews

--- routes/news.js
+++ routes/news.js
@@ -5,15 +5,18 @@ const { validateNews } = require('../validators/newsValidator');
 const { authenticatedUser } = require('../middlewares/authenticatedUser');
 const { verifyIsAdmin } = require('../middlewares/user.middelware');
 
+
+//List News
+router.get('/', newsController.getAllNews);
+
 //Detail News
-router.get('/:id', authenticatedUser , verifyIsAdmin , newsController.detailNews)
+router.get('/:id', authenticatedUser , verifyIsAdmin , newsController.detailNews);
 
 // Create News
 router.post('/', authenticatedUser , verifyIsAdmin , validateNews ,newsController.createNews);
 
-
 //Update News
-router.put('/:id', authenticatedUser , verifyIsAdmin , validateNews, newsController.editNews);
+router.put('/:id', authenticatedUser , verifyIsAdmin , validateNews, newsController.updateNews);
 
 //Delete News
 router.delete('/:id', authenticatedUser , verifyIsAdmin , newsController.deleteNews);

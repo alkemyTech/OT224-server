8d9553dc1424bb9d87a34bb4b8bf3f0335c5e3f1
SLRonzoni <slronzoni@gmail.com>
2022-07-04 19:01:17 -0300

    modified validator categories

--- validators/categoriesValidator.js
+++ validators/categoriesValidator.js
@@ -16,35 +16,13 @@ const validateCategories = [
         .withMessage('category name must contain at least 3 characters')
         .custom(async (name,{req,res})=>{  
                 const where = req.params.id  
-                                ? {name,id:{[Op.not]:req.params.id}  }
-                                : {name};
-
-                const isDuplicated = await Category.findOne({where ,attributes:['name','id']})  
-                console.log(isDuplicated)
+                ? {name,id:{[Op.not]:req.params.id}}
+                : {name};
+                let isDuplicated = await Category.findOne({where, attributes:['name','id']})  
                 if (isDuplicated) {
                     throw new Error('category name already exists')
                 } 
             }),   
-
-            // if(req.params.id){ 
-            //     const existId = await Category.findByPk(req.params.id,{attributes:['id']})          
-            //     if(name && existId!==null){                                  
-            //         const isDuplicated = await Category.findOne({ where: { name: name } ,attributes:['name','id']})                                                                          
-            //         if (isDuplicated!==null  && isDuplicated.name===name && isDuplicated.id !==Number(req.params.id)) {
-            //             throw new Error('category name already exists')
-            //         } 
-            //         return true 
-            //     }
-            // } else {
-            //     if(name){
-            //         const isDuplicated = await Category.findOne({ where: { name: name } ,attributes:['name']})
-            //         if (isDuplicated) {
-            //             throw new Error('category name already exists')
-            //         }
-            //         return true
-            //     }
-            // }
-        
     (req, res, next) => {
         validateResult(req, res, next)
     }

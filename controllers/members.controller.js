7148f1fff23d5f44cd17624989a936cc120799de
FlorenciaAGarrido <flhor.garrido@gmail.com>
2022-07-04 22:08:11 -0300

    feat(OT224-87): pagination for getAllMembers

--- controllers/members.controller.js
+++ controllers/members.controller.js
@@ -1,39 +1,41 @@
 const ModelMember = require('../models').Member;
+const ModelHelper = require("../helpers/modelHelper")
 
-const getAllMember = async(req, res) =>{
-    const members = await ModelMember.findAll()
+const getAllMember = async (req, res) => {
+
     try {
-        if(members){
-            res.send(members)
-        }           
+        const modelHelper = new ModelHelper(ModelMember)
+        const page = req.query.page || 1
+        const members = await modelHelper.findAndPaginate(page)
+        return res.status(200).json(members);
     } catch (error) {
-        res.status(400).send(error)
-        
+        res.status(500).send(error)
+
     }
 }
 
-const createMember = async (req, res) =>{
+const createMember = async (req, res) => {
     try {
-        const {name, facebookUrl, instagramUrl, linkedinUrl, image, description} =  req.body;
+        const { name, facebookUrl, instagramUrl, linkedinUrl, image, description } = req.body;
         const member = await ModelMember.create({
-            name : name,
+            name: name,
             facebookUrl: facebookUrl,
             instagramUrl: instagramUrl,
             linkedinUrl: linkedinUrl,
             image: image,
             description: description
         })
-        res.status(200).send({member})        
+        res.status(200).send({ member })
     } catch (error) {
-       res.status(500).send(error) 
+        res.status(500).send(error)
     }
 }
 
-const updateMember = async (req, res) =>{
+const updateMember = async (req, res) => {
     res.send('Hello from update member')
 }
 
-const deleteMember = async (req, res) =>{
+const deleteMember = async (req, res) => {
     res.send('Hello from delete member')
 }
 

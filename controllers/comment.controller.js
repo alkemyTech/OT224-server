ab8ce05dc5c7b9571d2ad8ca72dc4176a7ee89f7
hugotata78 <hugotatarinoff@gmail.com>
2022-07-06 15:01:25 -0300

    se creo controlador del endpoint para listar comentarios

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -1,7 +1,7 @@
 const CommentModel = require("../models").Comment;
 
 const createComment = async (req, res) => {
-    try {         
+    try {
         const comment = await CommentModel.create(req.body);
         return res.status(201).send(comment);
 
@@ -10,6 +10,19 @@ const createComment = async (req, res) => {
     }
 };
 
+const getAllComents = async (req, res) => {
+
+    try {
+        const comments = await CommentModel.findAll({
+            attributes:['body'],
+            order: [['createdAt', 'DESC']]
+        })
+        res.status(200).send({ comments })
+    } catch (error) {
+        res.status(500).send({ error })
+    }
+}
 module.exports = {
-    createComment
+    createComment,
+    getAllComents
 }

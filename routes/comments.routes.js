40a06ee7b09dd9f0d77f67276fa5a7cb420577c6
hugotata78 <hugotatarinoff@gmail.com>
2022-07-06 15:02:20 -0300

    se creo ruta del endpoint para listar comentarios

--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -8,5 +8,6 @@ const { verifyIsCommentsAdmin } = require('../middlewares/commentsAdminRole');
 
 // Create a new comment
 router.post('/', authenticatedUser, verifyIsCommentsAdmin, validateComments, commentController.createComment);
+router.get('/', authenticatedUser, verifyIsCommentsAdmin,  commentController.getAllComents);
 
 module.exports = router;

d75be8dd79963f4c5b32827e9ec2ca6b7bdbbdf7
SLRonzoni <slronzoni@gmail.com>
2022-07-07 16:30:35 -0300

    changing spaces and quotes

--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { verifyIsAdmin } = require("../middlewares/user.middelware");
-const { validateCategories } = require("../validators/categoriesValidator");
-const categoriesController = require("../controllers/categories.controller");
+const {verifyIsAdmin}=require('../middlewares/user.middelware');
+const {validateCategories}=require('../validators/categoriesValidator');
+const categoriesController=require('../controllers/categories.controller');
 
 /**
  * @swagger

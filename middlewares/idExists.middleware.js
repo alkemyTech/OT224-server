314188405de99d4549527ccf2450740cf820ea12
SLRonzoni <slronzoni@gmail.com>
2022-07-12 16:48:47 -0300

    modified switch and change send by json in function idExists

--- middlewares/idExists.middleware.js
+++ middlewares/idExists.middleware.js
@@ -5,7 +5,7 @@ const idExists = (req, res, next) => {
   const [,,url] = req.originalUrl.split('/');
   return getModel(id, url).then(model => {
     if (!model) {
-      return res.status(404).send('id not found');
+      return res.status(404).json('id not found');
     }
     next();
   });
@@ -14,9 +14,42 @@ const idExists = (req, res, next) => {
 const getModel = (id, url) => {
   let model;
   switch (url) {
+    case 'activity': 
+      model = models.Activity.findByPk(id);
+    break;
     case 'categories': 
       model = models.Categories.findByPk(id);
       break;
+    case 'comment': 
+      model = models.Comment.findByPk(id);
+    break;
+    case 'contacts': 
+      model = models.Contacts.findByPk(id);
+    break;
+    case 'member': 
+      model = models.Member.findByPk(id);
+    break;
+    case 'new': 
+      model = models.News.findByPk(id);
+    break;
+    case 'organization': 
+      model = models.Organization.findByPk(id);
+    break;
+    case 'role': 
+      model = models.Role.findByPk(id);
+    break;
+    case 'slide': 
+      model = models.Slide.findByPk(id);
+    break;
+    case 'testimonial': 
+      model = models.Testimonial.findByPk(id);
+    break;
+    case 'user': 
+      model = models.User.findByPk(id);
+    break;
+    default: 
+      model = null;
+    break;
   }
   return model;
 }

c1ae95e48367b5ba2604c8cd23604366406a2009
Victor <victoramarill13@gmail.com>
2022-07-16 14:53:02 -0300

    Normalizacion de users.controllers

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,109 +1,65 @@
-const ModeloUser = require('../models').User;
-const bcrypt = require('bcrypt')
-const { validationResult } = require('express-validator')
-const {welcomeEmail}=require('../services/welcomeEmail')
-const { tokenGenerator } = require('../helpers/tokenGenerator')
+const ModeloUser = require("../models").User;
+const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
+const { welcomeEmail } = require("../services/welcomeEmail");
+const { tokenGenerator } = require("../helpers/tokenGenerator");
+const BaseController = require("./base.controller");
 
 const getAllUsers = async (req, res) => {
-    try {
-        const users = await ModeloUser.findAll()
-        if (!users) {
-            res.status(400).json({
-                msg: 'users not found'
-            })
-        } else {
-            res.status(200).send(users)
-        }
-    } catch (error) {
-        res.status(500).send(error)
-    }
-}
-
-const createUser = async (req, res) => {
-    try {
-        const { firstName, lastName, email, password, photo, roleId } = req.body
-
-        const errors = validationResult(req)
-
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() })
-        }
-        const pass = bcrypt.hashSync(password, parseInt(process.env.SALT))
-
-        const user = await ModeloUser.create({
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            photo: photo,
-            password: pass,
-            roleId: roleId
-        })
-
-        const token = tokenGenerator(user)
-    
-        const emailSent=await welcomeEmail(user)
-        res.status(200).json({ newUser: user, emailSent, token:token})
-
-    } catch (error) {
-        res.status(500).json({ error: error })
-    }
-}
-
-const updateUser = async (req, res) => {
-    const editUser = await db.ModeloUser.findByPk(req.params.id, {
-        include: [{ association: 'roles' }]
-    })
-    try {
-        if (editUser) {
-            editUser.update({
-                firstName: !req.body.firstName ? editUser.firstName : req.body.firstName,
-                lastName: !req.body.lastName ? editUser.lastName : req.body.lastName,
-                email: !req.body.email ? editUser.email : req.body.email,
-                image: !req.file ? editUser.file : req.file.filename,
-            })
-        } else {
-            res.status(404).json({
-                msg: 'No se encontro el usuario',
-                status: 404
-            })
-        }
-    } catch (err) { console.log(err) }
-}
-
-const deleteUser = async (req, res) => {
-    try {
-        const user = await ModeloUser.findOne({where : {id : req.params.id}})
-        if(user){
-            await ModeloUser.destroy({
-                where: {id: req.params.id}
-            })
-            res.status(200).send("User deleted succefuly")
-        }else{
-            res.status(404).send("User does not exist")
-        }
-     } catch (error) {
-        res.status(500).send(error)
-     }
-}
-
-const findMe = async (req, res) => {
-    return res.status(200).json(req.user);
+  return BaseController.getAllModels(req, res, ModeloUser);
 };
 
-const getUserId= async (req, res)=>{
-    try {
-        const user = await ModeloUser.findByPk(req.params.id)
-        user ? res.status(200).send(user) : res.sendStatus(404)
-    } catch (error) {
-        res.status(500).send(error)
+const createUser = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password, photo, roleId } = req.body;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-}
+    const pass = bcrypt.hashSync(password, parseInt(process.env.SALT));
+
+    const user = await ModeloUser.create({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      photo: photo,
+      password: pass,
+      roleId: roleId,
+    });
+
+    const token = tokenGenerator(user);
+
+    const emailSent = await welcomeEmail(user);
+    res.status(200).json({ newUser: user, emailSent, token: token });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+const updateUser = async (req, res) => {
+  let inputVars = req.body;
+  return BaseController.updateModel(req, res, ModeloUser, inputVars);
+};
+
+const deleteUser = async (req, res) => {
+  return BaseController.deleteModel(req, res, ModeloUser);
+};
+
+const findMe = async (req, res) => {
+  return res.status(200).json(req.user);
+};
+
+const getUserId = async (req, res) => {
+  return BaseController.getModelById(req, res, ModeloUser);
+};
 
 module.exports = {
-    getAllUsers,
-    createUser,
-    updateUser,
-    deleteUser,
-    findMe,
-    getUserId
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+  findMe,
+  getUserId,
 };

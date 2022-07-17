70fded5a827cba3137938d630691e005680dbf09
César Muzio <cmuzio375@gmail.com>
2022-07-17 19:12:57 -0300

    base controller applied

--- controllers/organization.controller.js
+++ controllers/organization.controller.js
@@ -1,33 +1,29 @@
 const OrganizationModel = require("../models").Organization;
 const { Slide } = require("../models");
+const baseController = require('./base.controller')
 
 
 //Get All Organizations
 const getOrganizations = async ( req, res ) => {
-
-    try {
-        const organizations = await OrganizationModel.findAll()
-
-        res.status(200).send({
-            organizations
-        })
-
-    } catch (error) {
-        console.log( error )
-        res.status(500).send( error )
-    }
-
+    return baseController.getAllModels( req, res, OrganizationModel )
 }
 
 //Create and save an organization
-const createOrganization = async (req, res) => {    
-    try {
-        let organization = req.body;        
-        organization = await OrganizationModel.create (organization);        
-        res.status(201).send(organization);           
-    } catch (error) {
-        res.status(500).send(error);
+const createOrganization = async (req, res) => {  
+
+    const { name, image, address, phone, email, welcomeText, aboutUsText } = req.body;
+    let data = {
+        name,
+        image,
+        address,
+        phone,
+        email,
+        welcomeText,
+        aboutUsText
     }
+
+    return baseController.createModel( res, OrganizationModel, data )
+
 }
 // Get Organization
 const getOrganizationById = async(req, res) => {    
@@ -60,51 +56,14 @@ const getOrganizationById = async(req, res) => {
 
 //Update organization
 const updateOrganization = async (req, res) => {  
-    try {
-        const id = req.params.id
-        const organization = await OrganizationModel.findByPk(id);
-        if(!organization){
-            res.status(404).send({message: "Organization no found!"})
-        }else{
-            const updateOrganization = await OrganizationModel.update(req.body,{ where: { id } });
-            
-            if(updateOrganization==1){
-                res.status(201).send({message: "Organization updated"});
-            }
-        }        
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(error)
-    }    
+    const data = req.body;
+    return baseController.updateModel( req, res, OrganizationModel, data)
 }
 
 //Delete Organization
 const deleteOrganization = async( req, res ) => {
-
-    const id = req.params.id;
-
-
-     try {
-
-        const org = await OrganizationModel.findByPk(id)
-
-        if(org === null){
-            return res.status(400).send({
-                msg: 'Invalid organization id'
-            })
-        }else{
-            await OrganizationModel.destroy({
-                where: { id }
-            })
-
-            res.status(200).send()
-        }
-        
-    } catch (error) {
-        console.log( error )
-        res.status(500).send( error )
-    }
- 
+    return baseController.deleteModel( req, res, OrganizationModel )
+    
 }
 
 module.exports = {

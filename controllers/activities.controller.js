527fb599f5e2e9b148448ff94f3caa1ac7aa9978
FlorenciaAGarrido <flhor.garrido@gmail.com>
2022-07-19 14:00:15 -0300

    fix(OT224-139):normalize endpoints activities

--- controllers/activities.controller.js
+++ controllers/activities.controller.js
@@ -8,17 +8,22 @@ const {
 
 const ActivityModel = require("../models").Activity;
 
+const createActivity = async (req, res) => { 
+	const { name, content, image } = req.body;
+	await createModel(res, ActivityModel, { name, content, image });
+}
+
 const getAllActivities = async (req, res) => 
 	await getAllModels(req, res, ActivityModel);
 
 const getActivityById = async (req, res) => 
 	await getModelById(req, res, ActivityModel);
 
-const createActivity = async (req, res) => 
-	await createModel(res, ActivityModel, req.body);
-
-const updateActivity = async (req, res) => 
-	await updateModel(req, res, ActivityModel, req.body);
+const updateActivity = async (req, res) => {
+	const { name, content, image } = req.body;
+	await updateModel(req, res, ActivityModel, { name, content, image });
+}
+	
 
 const deleteActivity = async (req, res) => 
 	await deleteModel(req, res, ActivityModel);
